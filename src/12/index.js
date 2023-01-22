82b30fa096433d0647ad784afa923cd95b5df106
Li Yang <cdjv_liyang@163.com>
2023-01-22 20:21:11 +0800

    feat: 调式npm script

--- src/12/index.js
+++ src/12/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 (async function () {
   const content = await fs.readFile(path.resolve(__dirname, './test.jsonc'), { encoding: 'utf-8' });
-
+content;
   console.log(content);
 
   fs.writeFile(path.resolve(__dirname, './test.jsonc'), content, { encoding: 'utf-8' });
