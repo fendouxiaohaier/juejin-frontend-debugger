d6d44a3301a0a54c77fb27ee566dd31939ff51df
Li Yang <cdjv_liyang@163.com>
2023-01-30 13:50:34 +0800

    feat: 性能分析

---
+++ src/23/debugger-ant-design/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Button, Input} from 'antd/dist/antd';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">按钮</Button>
+      <Input placeholder="Basic usage" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/26/worker-performance-optimization/worker.js
@@ -0,0 +1,11 @@
+addEventListener('message', (event) => {
+
+  console.log('typeof event.data:', typeof event.data);
+
+  let a = 1;
+  for (let i = 0; i < event.data; i++) {
+    a += 1;
+  }
+
+  postMessage(a);
+});
